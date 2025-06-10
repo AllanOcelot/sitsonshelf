3ac0ec809972988632423fe09c3022355ced267d
Allan <workofcode@gmail.com>
2025-06-10 21:29:04 +0100

    - Think I've got the hang of docker now, please correct if wrong

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -15,4 +15,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0', // ← this allows external access from your host machine
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    }
+  }
 })
