f92a53664bdefa919f73283c47982fe84e3557a8
Allan <workofcode@gmail.com>
2025-06-10 18:48:23 +0100

    - Init repo with folder stucture

---
+++ README.md
@@ -0,0 +1,19 @@
+### Sits On Shelf
+A simple locally hosted web app to help organise your physical media.
+Be it records, movies, books or well anything.
+
+
+## Why?
+Because I enjoy keeping track of things in discogs, but don't really enjoy the UX of that site, Then I thought 'eh, I'll give it a go' so here we are.
+
+
+## tech stack
+- docker
+- node
+- vue3/vite
+- node 
+- pocketbase 
+
+## Development will be slow
+My life is busy, this is just something I plan to work on with my free time
+
---
+++ Server/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+app.use(express.json());
+
+// change to w.e pb will run on locally
+const POCKETBASE_URL = "http://pocketbase:8090";
+
+// Proxy GET request
+app.get("/api/media", async (req, res) => {
+  try {
+    const response = await fetch(`${POCKETBASE_URL}/api/collections/media/records`);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Failed to fetch media:", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('There is nothing in the desert and no man needs nothing.')
+});
